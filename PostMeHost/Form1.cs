a517126033c2eb4491bc233daf57467526eb1dcc
Anmol Saxena <anmol.saxena10@gmail.com>
2019-03-17 19:54:19 +0530

    config added

--- PostMeHost/Form1.cs
+++ PostMeHost/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,17 @@ namespace PostMeHost
 {
     public partial class Form1 : Form
     {
+        ServiceHost sh = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sh = new ServiceHost(typeof(PostMeService.PostMeService));
+            sh.Open();
+            label1.Text = "Service Running!!! Start Posting";
         }
     }
 }
